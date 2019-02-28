a88ec4fad852c1d3e099fec025c9733ec5943abb
Punit Shah <punit.shah@bbc.co.uk>
2019-02-28 23:26:06 +0000

    Updates

---
+++ precache-manifest.8e3aa983d5c0699f5ef2ee37f8b46f1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25b9ebc1c24702f40a13",
+    "url": "/asteroids/static/js/runtime~main.25b9ebc1.js"
+  },
+  {
+    "revision": "081b8357b714980b9b7e",
+    "url": "/asteroids/static/js/main.081b8357.chunk.js"
+  },
+  {
+    "revision": "ca97c99606a900eeec6a",
+    "url": "/asteroids/static/js/2.ca97c996.chunk.js"
+  },
+  {
+    "revision": "081b8357b714980b9b7e",
+    "url": "/asteroids/static/css/main.305f154e.chunk.css"
+  },
+  {
+    "revision": "3a8f2476ce0360a49d90177e41f93127",
+    "url": "/asteroids/index.html"
+  }
+];
